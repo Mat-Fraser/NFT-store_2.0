3d79092ed95496bb7bd5b311bc8885b42eec20be
Mat-Fraser <86045021+Mat-Fraser@users.noreply.github.com>
2022-09-10 20:40:07 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,92 @@
+const products = [
+  {
+    id: 'price_1LgNhXSFGhehSLFMQBGYiKpv',
+    name: 'Ape-NFT-087',
+    price: 13000,
+    currency: 'INR',
+    image: '/plants/Ape-NFT-087.jpg',
+    rating: {
+      count: 85,
+      rate: 4.5,
+    },
+  },
+  {
+    id: 'price_1LgNgISFGhehSLFM07dNLsvQ',
+    name: 'Astronaut-903',
+    price: 15000,
+    currency: 'INR',
+    image: '/plants/Astronaut-903.jpg',
+    rating: {
+      count: 137,
+      rate: 4.5,
+    },
+  },
+  {
+    id: 'price_1LgNfgSFGhehSLFMXPWo6w1w',
+    name: 'Astronaut-389',
+    price: 18000,
+    currency: 'INR',
+    image: '/plants/Astronaut-389.jpg',
+    rating: {
+      count: 104,
+      rate: 4,
+    },
+  },
+  {
+    id: 'price_1LgNfHSFGhehSLFMsZkrNk8c',
+    name: 'Astronaut-701',
+    price: 28000,
+    currency: 'INR',
+    image: '/plants/Astronaut-701.jpg',
+    rating: {
+      count: 213,
+      rate: 5,
+    },
+  },
+  {
+    id: 'price_1LgNeZSFGhehSLFMl9rvyHVz',
+    name: 'Ape-NFT-sigma',
+    price: 9700,
+    currency: 'INR',
+    image: '/plants/Ape-NFT-sigma.jpeg',
+    rating: {
+      count: 78,
+      rate: 4,
+    },
+  },
+  {
+    id: 'price_1LgNdvSFGhehSLFMt69FRzLn',
+    name: 'Ape-NFT-beta',
+    price: 8000,
+    currency: 'INR',
+    image: '/plants/Ape-NFT-beta.jpg',
+    rating: {
+      count: 89,
+      rate: 5,
+    },
+  },
+  {
+    id: 'price_1LgNdLSFGhehSLFMvRwoP7wI',
+    name: 'Ape-NFT-delta',
+    price: 7000,
+    currency: 'INR',
+    image: '/plants/Ape-NFT-delta.jpg',
+    rating: {
+      count: 271,
+      rate: 5,
+    },
+  },
+  {
+    id: 'price_1LgNceSFGhehSLFMq0EoWh7J',
+    name: 'Ape-NFT-alpha',
+    price: 12000,
+    currency: 'INR',
+    image: '/plants/Ape-NFT-alpha.jpg',
+    rating: {
+      count: 78,
+      rate: 4,
+    },
+  },
+];
+
+export default products;
